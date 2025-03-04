28b24146fda1647a5d5f833e32e4fd384d263e31
hugo9442 <hugo9442@gmail.com>
2025-03-04 20:14:13 +0000

    responsive check

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -1,45 +1,49 @@
 const Cards = () => {
-  return (<div className="CardsName">
-    <div className="card1">
-      <div ClassName="card" >
-        <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Find out More</a>
+  return (
+    <div className="container text-center" style={{ width: "80%" }}>
+      <div class="row align-items-start">
+        <div className="col">
+          <div ClassName="card" >
+            <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Find out More</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div ClassName="card" >
+            <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Find out More</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div ClassName="card" >
+            <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Find out More</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div ClassName="card" >
+            <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Find out More</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div className="card1">
-      <div ClassName="card" >
-        <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Find out More</a>
-        </div>
-      </div>
-    </div>
-    <div className="card1">
-      <div ClassName="card" >
-        <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Find out More</a>
-        </div>
-      </div>
-    </div>
-    <div className="card1">
-      <div ClassName="card" >
-        <img src="src/img/rigo-baby.jpg" class="card-img-top" alt="src/img/rigo-baby.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Find out More</a>
-        </div>
-      </div>
-    </div>
-  </div>);
+  );
 }
 export default Cards;
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,25 +1,33 @@
 const Navbar = () => {
 
-    return (<nav className="navbar" data-bs-theme="dark">
-        <p className="text">Start Bootstrap</p>
-        <div className="ulNav">
-            <ul className="nav justify-content-end">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
-                </li>
-                <li clasNameclassName="nav-item">
-                    <a className="nav-link" href="#">Service</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" >
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar scroll</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="ulNav">
+                    <div className="collapse navbar-collapse" id="navbarScroll">
 
+                        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll justify-content-end" style={{ "--bs-scroll-height": "100px" }}>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Service</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
